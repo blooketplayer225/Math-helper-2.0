7238625f796331e068dcc4347e1264df9792e7c2
blooketplayer225 <will.james.platt@gmail.com>
2026-03-25 16:58:33 -0500

    Add configuration for Scramjet service

--- static/config.js
+++ static/config.js
@@ -1,1 +1,9 @@
-let _CONFIG = {};
+window.__scramjet$config = {
+  prefix: '/service/', // This is the URL prefix for proxied sites
+  bare: 'https://bare.benroberts.dev/', // A fast, public Bare server
+  codec: window.self.scramjet.codecs.xor, // Encodes URLs to hide them from filters
+  handler: '/scram/scramjet.handler.js',
+  bundle: '/scram/scramjet.bundle.js',
+  config: '/scram/scramjet.config.js',
+  sw: '/scram/scramjet.sw.js',
+};
